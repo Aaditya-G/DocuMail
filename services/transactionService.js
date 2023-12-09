f11af8b182d6c65b517c1aec1b4c3f5e3c2f0073
Aaditya-G <aadityagupta400@gmail.com>
2023-12-09 13:37:04 +0530

    feat: incorporates checking for format of emails and dates sent in request

--- services/transactionService.js
+++ services/transactionService.js
@@ -7,8 +7,22 @@ exports.handleTransactionPdfGeneration = async (email, startDate, endDate) => {
     throw new Error("Missing required start date and end Date in body");
   }
 
+  const dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
+
+  if (!dateFormat.test(startDate) || !dateFormat.test(endDate)) {
+    throw new Error(
+      "Invalid format for start date or end date. Required format: 'YYYY-MM-DDTHH:mm:ss.sssZ'"
+    );
+  }
+
   if (!email) {
     throw new Error("Missing required user-email in header");
+  }
+
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+  if (!emailRegex.test(email)) {
+    throw new Error("Invalid email format");
   }
 
   if (typeof startDate !== "object" || typeof endDate !== "object") {
